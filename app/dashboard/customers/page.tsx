0838f049ea0f6b0b3b5be6e0aaa458f6a8c29d58
PONNARAMESH <ponnaramesh2016@gmail.com>
2024-10-07 15:52:42 +0530

    feat: working on loading & dynamic pages

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
-export default function Page() {
+import { fetchRevenue } from "@/app/lib/data";
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const p1 = () => {
+    setTimeout(() => {
+      return 1
+    }, 3000);
+  };
+  const res  = await p1();
   return <p>Customers Page</p>;
 }
